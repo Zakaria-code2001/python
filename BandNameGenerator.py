bf0565ccfbef8966a31ac5ad83239b7e3b6c33d3
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-07-24 14:13:49 +0200

    Day1

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name Generator.");
+city = input("whcih city di. yoyu grow up in?");
+PetName = input("give the name of a pet?");
+print(city + ' ' + PetName + '\n');
---
+++ README.md
@@ -0,0 +1,2 @@
+This repo will be about my 100 days Python Udemy course
+Day_1 Project : Band NAme Generator 
